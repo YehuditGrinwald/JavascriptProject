ca008dba4b9867b05ab8c1f2f493c8c380ef1b53
100yehudit <1000yehudit@gmail.com>
2017-03-05 12:39:42 +0200

    files

--- JOOMI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace JOOMI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
